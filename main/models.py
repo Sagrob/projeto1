8107fffd13c3eefd1abeb2930fee521cc450e3df
Sagrob <bieldiniz27@gmail.com>
2024-05-15 21:27:03 -0300

    nova adição em views

--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    name = models.CharField(max_length=200)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    create_at= models.DateTimeField(auto_now=True)
+
+
+class Item(models.Model):
+    updated_at = models.DateTimeField(auto_now_add=True)
+    create_at = models.DateTimeField(auto_now=True)
+    todo = models.ForeignKey(Todo, on_delete=models.CASCADE, related_name="items")
+    text = models.TextField()
+    complete = models.BooleanField(default=False)
+
+def __str__(self):
+    return f"{self.id} | {self.text}"
